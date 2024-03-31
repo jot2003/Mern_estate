ce6e98085990c53056362d8e5f04c5596928dbfb
jot2003 <hoangkimtrithanh@gmail.com>
2024-03-31 12:04:27 +0700

    update the header and make the profile page private

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -19,7 +19,7 @@ export default function OAuth() {
                 headers: { 
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo: result.user.photo}),
+                body: JSON.stringify({name: result.user.displayName, email: result.user.email, photo: result.user.photoURL}),
             })
             const data = await res.json()
             dispatch(signInSuccess(data));
